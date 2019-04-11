ef81a26283f0e0da9d243a1bb4e19fdcfce800d1
Fisnik Limani <fisnik.limani1@gmail.com>
2019-04-11 18:50:03 +0200

    lab2 tasks&solutions added

---
+++ Lab2/Lab2_Zad3/Lab2_Zad3/Najava.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab2_Zad3
+{
+    public partial class Najava : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+                ViewState["obidi"] = 0;
+        }
+
+        protected void btnPodnesi_Click(object sender, EventArgs e)
+        {
+            if(txtKorisnik.Text != "" && txtLozinka.Text != "")
+            {
+                int obidi = int.Parse(ViewState["obidi"].ToString());
+                if(txtLozinka.Text == "mp")
+                {
+                    Session["korisnik"] = txtKorisnik.Text;
+                    Response.Redirect("~/GlavnaStranica.aspx");
+                }
+                else if(obidi == 3)
+                {
+                    btnPodnesi.Enabled = false;
+                    lblObidi.Text = "Go imash nadminato kvotata za dozvoleni obidi!";
+                }
+                else
+                {
+                    obidi++;
+                    lblObidi.Text = "Broj na obidi:" + obidi;
+                    ViewState["obidi"] = obidi;
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,9 +3,11 @@
 ##### [COURSE](https://finki.ukim.mk/mk/subject/%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%BD%D0%B5%D1%82-%D1%82%D0%B5%D1%85%D0%BD%D0%BE%D0%BB%D0%BE%D0%B3%D0%B8%D0%B8) - Internet Technologies
 <br/>
 <br/>
+
 ### CONTENT
 
 Lab | Tasks & Solutions 
 ------------ | ------------ 
 LAB1 | [LAB1 TASKS & SOLUTIONS](https://github.com/FisnikL/Internet-Tehnologii/tree/master/LAB1) 
+LAB2 | [LAB2 TASKS & SOLUTIONS](https://github.com/FisnikL/Internet-Tehnologii/tree/master/LAB2) 
 
